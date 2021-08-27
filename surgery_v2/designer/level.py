95a7dcb25072d77f9d4c3e46e280ee70b65ecfad
Julian Lorenz <lorenz@fzi.de>
2021-08-27 14:43:41 +0200

    Diamanten banger kette

--- surgery_v2/designer/level.py
+++ surgery_v2/designer/level.py
@@ -31,8 +31,9 @@ def wound_idx_2_str(idx: int):
         3: "scalpel",
         4: "scar",
         5: "small",
-        6: "swollen",
-        7: "swollen_infected"
+        6: "stitched",
+        7: "swollen",
+        8: "swollen_infected"
     }.get(idx, "")
 
 
@@ -44,9 +45,18 @@ def wound_str_2_idx(name: str):
         "scalpel": 3,
         "scar": 4,
         "small": 5,
-        "swollen": 6,
-        "swollen_infected": 7
+        "stitched": 6,
+        "swollen": 7,
+        "swollen_infected": 8
     }.get(name, -1)
+
+
+def convert_scale(src_n):
+    return {
+        "infected.png": 0.617801047,
+        "small.png": 0.478991597,
+        "scalpel.png": 0.471204188,
+    }.get(src_n, 1)
 
 
 def get_patch_file(idx: int):
@@ -117,7 +127,7 @@ class Patient:
             if bp.wound_files:
                 draw.rounded_rectangle([(int(name[2]), int(name[3])),
                                         (int(name[2]) + 74, int(name[3]) + 74)],
-                                       13, None, outline=(88, 95, 52, 255), width=3)
+                                       13, fill=None, outline=(214, 241, 139), width=3)
         return canvas
 
     def get_canvas_mask(self):
@@ -143,10 +153,11 @@ class BodyPart:
         4: [(526, 220), (156, 170), (158, 387), (526, 320)]
     }
     color_map = {
-        9: np.array([255, 255, 255], dtype=np.uint8),
-        8: np.array([0, 0, 0], dtype=np.uint8),
-        7: np.array([63, 81, 181], dtype=np.uint8),
-        6: np.array([3, 169, 244], dtype=np.uint8),
+        10: np.array([255, 255, 255], dtype=np.uint8),
+        9: np.array([0, 0, 0], dtype=np.uint8),
+        8: np.array([63, 81, 181], dtype=np.uint8),
+        7: np.array([3, 169, 244], dtype=np.uint8),
+        6: np.array([169, 3, 244], dtype=np.uint8),
         5: np.array([0, 150, 136], dtype=np.uint8),
         4: np.array([139, 195, 74], dtype=np.uint8),
         3: np.array([255, 235, 59], dtype=np.uint8),
@@ -155,7 +166,8 @@ class BodyPart:
         0: np.array([96, 125, 139], dtype=np.uint8),
     }
     wound_progress = {
-        "big.png": "scar",
+        "big.png": "stitched",
+        "stitched.png": "scar",
         "bruise.png": None,
         "infected.png": "big",
         "scalpel.png": "big",
@@ -205,8 +217,11 @@ class BodyPart:
         canvas = Image.open(self.body_part_canvas)
         for wf, wd in zip(self.wound_files, self.wound_data):
             img = Image.open(wf)
+            if "scar.png" in wf:
+                img = img.resize((int(img.width * wd["scale"]), int(img.height)))
+            else:
+                img = img.resize((int(img.width * wd["scale"]), int(img.height * wd["scale"])))
             img = img.rotate(wd["rotation"], expand=True)
-            img = img.resize((int(img.width * wd["scale"]), int(img.height * wd["scale"])))
             # Coords are in [y, x, height, width] order
             img_blend = Image.new('RGBA', canvas.size)
             img_blend.paste(img, (wd["coords"][1], wd["coords"][0]), img.split()[-1])
@@ -224,7 +239,7 @@ class BodyPart:
         return Image.fromarray(mask_rgb)
 
     def canvas_mask_to_ndarray(self):
-        mask_nd = np.zeros([*self.m_canvas_np.shape, 10], dtype=np.uint8)
+        mask_nd = np.zeros([*self.m_canvas_np.shape, 11], dtype=np.uint8)
         for key in self.color_map:
             mask_nd[self.m_canvas_np == key, key] = 1
         return mask_nd
@@ -255,7 +270,20 @@ class BodyPart:
             return False
         next_idx = wound_str_2_idx(progress)
         next_wound = get_wound_patch(next_idx)
+        self.wound_data[idx]["scale"] *= convert_scale(os.path.split(self.wound_files[idx])[1])
+        old_bbox = self.wound_data[idx]["coords"]  # y, x, h, w
+        img = Image.open(next_wound)
+        if "scar.png" in next_wound or "stitched.png" in next_wound:
+            img = img.resize((int(img.width * self.wound_data[idx]["scale"]), int(img.height)))
+        else:
+            img = img.resize(
+                (int(img.width * self.wound_data[idx]["scale"]), int(img.height * self.wound_data[idx]["scale"])))
+        img = img.rotate(self.wound_data[idx]["rotation"], expand=True)
+        new_bbox = [old_bbox[0] + (old_bbox[2] - img.height) // 2,
+                    old_bbox[1] + (old_bbox[3] - img.width) // 2,
+                    img.height, img.width]
         self.wound_files[idx] = next_wound
+        self.wound_data[idx]["coords"] = new_bbox
         return False
 
     def _find_places_for_wound(self, f_wound):
@@ -266,12 +294,17 @@ class BodyPart:
         scale = 1.0
         for iteration in range(1000):
             rotation = angle = random.randint(0, 360)
-            img = w_img.copy().rotate(angle, expand=True)
-
-            if os.path.split(f_wound)[1] in ["big.png", "infected.png", "scar.png"]:
+            if os.path.split(f_wound)[1] in ["big.png", "infected.png"]:
                 # Scale is allowed
                 scale = random.uniform(0.5, 1.5)
-                img = img.resize((int(img.width * scale), int(img.height * scale)))
+                img = w_img.copy().resize((int(w_img.width * scale), int(w_img.height * scale)))
+            elif os.path.split(f_wound)[1] in ["scar.png", "stitched.png"]:
+                scale = random.uniform(0.5, 1.5)
+                img = w_img.copy().resize((int(w_img.width * scale), int(w_img.height)))
+            else:
+                img = w_img.copy()
+            img = img.rotate(angle, expand=True)
+
             img = np.array(img.split()[-1])
             # img[img > 0] = wound_str_2_idx(os.path.split(f_wound)[1].split(".")[0])
             THRESH = 50
